7f3f39cbbd6856203b843828f01967c69cedfa5b
talex7 <talexanian@gmail.com>
2016-11-01 12:54:45 -0400

    Initial Commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Thomas Alexanian on 2016-11-01.
+//  Copyright © 2016 Thomas Alexanian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
